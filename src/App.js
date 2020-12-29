7970b6077b6b8e780698f8b5a4c986991df05509
Ben Thompson <btomo86@gmail.com>
2020-12-29 15:36:29 +0000

    added routes to only render the component we want

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStlye";
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route>
+          <ContactUs path="/contact" exact />
+        </Route>
+      </Switch>
     </div>
   );
 }
