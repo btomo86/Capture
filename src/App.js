35eb06e5e74cac61ba99c9097fcbdefdb5487f58
Ben Thompson <btomo86@gmail.com>
2020-12-29 10:31:57 +0000

    did some styling using styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import GlobalStyle from "./components/GlobalStlye";
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      <GlobalStyle />
     </div>
   );
 }
