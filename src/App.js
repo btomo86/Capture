4701706c95247529b7a7dfed24c950d742bf70a3
Ben Thompson <btomo86@gmail.com>
2020-12-30 09:30:47 +0000

    did some updating on ourwork section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 import { Switch, Route } from "react-router-dom";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route>
           <ContactUs path="/contact" exact />
