8e5ea11c25f44c22f7ef44568580f6e25bcf5590
Ben Thompson <btomo86@gmail.com>
2020-12-31 12:14:58 +0000

    added a reuseable component for toggle on the faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Layout } from "../Styles";
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
   return (
@@ -8,50 +9,60 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            lorem ipsum, dolor sit amet consectetur adipiscing
-            Necessitatibus,neque.
-          </p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+
+            <p>
+              lorem ipsum, dolor sit amet consectetur adipiscing
+              Necessitatibus,neque.
+            </p>
+          </div>
+
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            lorem ipsum, dolor sit amet consectetur adipiscing
-            Necessitatibus,neque.
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              lorem ipsum, dolor sit amet consectetur adipiscing
+              Necessitatibus,neque.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            lorem ipsum, dolor sit amet consectetur adipiscing
-            Necessitatibus,neque.
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Different Payment Methods</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              lorem ipsum, dolor sit amet consectetur adipiscing
+              Necessitatibus,neque.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What products do you offer</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            lorem ipsum, dolor sit amet consectetur adipiscing
-            Necessitatibus,neque.
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>What products do you offer</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              lorem ipsum, dolor sit amet consectetur adipiscing
+              Necessitatibus,neque.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
     </Faq>
   );
 };
