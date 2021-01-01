aed2a7b36edb28e2bd36d4d7cfcbcbddea03d7bc
Ben Thompson <btomo86@gmail.com>
2021-01-01 00:51:00 +0000

    added media queries to make it mobile friendly

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -76,6 +76,9 @@ const Cards = styled.div`
   @media (max-width: 1300px) {
     justify-content: center;
   }
+  @media (max-width: 1300px) {
+    justify-content: center;
+  }
 `;
 const Card = styled.div`
   flex-basis: 15rem;
